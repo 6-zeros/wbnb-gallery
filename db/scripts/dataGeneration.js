7eafcb2d1d47ae4de0108abcce5810be40e3e978
James Tsukamoto <jaytsuk@gmail.com>
2019-01-10 20:06:55 -0800

    Refactor data generation script & create db seeding script
    In this commit, I've refactored my data generation script to
    create n files for j total entires. This successfully generates
    10 million pieces of data.
    
    Additionally, I've created a script to programatically populate
    my postgreSQL database with all entries.

--- db/scripts/dataGeneration.js
+++ db/scripts/dataGeneration.js
@@ -3,9 +3,9 @@ const faker = require('faker');
 const path = require('path');
 
 let currentId = 1;
-let errors = 0;
+let currentFile = 1;
+let firstRow = true;
 const start = new Date().getTime();
-console.log('Start: ', start);
 
 const generateRandomNumber = (min, max) => {
   let randomNumber = Math.round(Math.random() * max);
@@ -15,11 +15,11 @@ const generateRandomNumber = (min, max) => {
   return randomNumber;
 };
 
-
 const jsonGen = (id) => {
   const jsonObject = {
     roomId: id,
     roomname: `room${id}`,
+    uniqId: `uniq${id}${faker.random.number()}`,
     photo: [
       { url: `http://dx37dhl9dvhoj.cloudfront.net/img${generateRandomNumber(1, 981)}.jpg`, caption: faker.lorem.sentence() },
       { url: `http://dx37dhl9dvhoj.cloudfront.net/img${generateRandomNumber(1, 981)}.jpg`, caption: faker.lorem.sentence() },
@@ -28,51 +28,69 @@ const jsonGen = (id) => {
       { url: `http://dx37dhl9dvhoj.cloudfront.net/img${generateRandomNumber(1, 981)}.jpg`, caption: faker.lorem.sentence() },
     ],
   };
-
-  currentId += 1;
   return jsonObject;
 };
 
 const csvGen = (id) => {
   const csvRow = id === 1
-    ? `roomid, roomname \n${id}, roomname${id} \n`
-    : `${id}, roomname${id} \n`;
-
-  currentId += 1;
+    ? `roomid,roomname\n${id},roomname${id},uniq${id}${faker.random.number()}\n`
+    : `${id},roomname${id},uniqId\n`;
   return csvRow;
 };
 
 const csvGen2 = (id) => {
-  const outputRow = `${id}, roomname${id}, http://dx37dhl9dvhoj.cloudfront.net/img${generateRandomNumber(1, 981)}.jpg, ${faker.lorem.sentence()}\n${id}, roomname${id}, http://dx37dhl9dvhoj.cloudfront.net/img${generateRandomNumber(1, 981)}.jpg, ${faker.lorem.sentence()}\n${id}, roomname${id}, http://dx37dhl9dvhoj.cloudfront.net/img${generateRandomNumber(1, 981)}.jpg, ${faker.lorem.sentence()}\n${id}, roomname${id}, http://dx37dhl9dvhoj.cloudfront.net/img${generateRandomNumber(1, 981)}.jpg, ${faker.lorem.sentence()}\n${id}, roomname${id}, http://dx37dhl9dvhoj.cloudfront.net/img${generateRandomNumber(1, 981)}.jpg, ${faker.lorem.sentence()}\n`;
+  let outputRow = '';
 
-  const csvRow = id === 1
-    ? `roomid, roomname, photoUrl, photoCaption\n${outputRow}`
+  for (let i = 1; i < 6; i += 1) {
+    outputRow += `${id},`;
+    outputRow += `roomname${id}${i},`;
+    outputRow += `uniq${id}${faker.random.number()},`;
+    outputRow += `http://dx37dhl9dvhoj.cloudfront.net/img${generateRandomNumber(1, 981)}.jpg,`;
+    outputRow += `${faker.lorem.sentence()}\n`;
+  }
+
+  const csvRow = firstRow
+    ? `roomid,roomname,uniqId,photoUrl,photoCaption\n${outputRow}`
     : `${outputRow}`;
-  currentId += 1;
   return csvRow;
 };
 
-const writeDataToFile = (totalEntries) => {
-  const fileWriteStream = fs.createWriteStream(path.join(__dirname, '/data/gallerydata2.csv'));
+const createFiles = (totalEntries, entriesPerFile) => {
+  // JSON: `/data/alldata/alldata${currentFile}.txt`
+  // Rooms CSV: `/data/roomsdata/roomsdata${currentFile}.csv`
+  // Photos CSV: `/data/gallerydata/gallerydata${currentFile}.csv`
+  let fileWriteStream = fs.createWriteStream(path.join(__dirname, `/data/gallerydata/gallerydata${currentFile}.csv`));
 
-  const writeChunksToFile = (timesRun) => {
-    if (currentId >= totalEntries) {
+  const writeToFile = () => {
+    if (((currentId - 1) % entriesPerFile === 0) && currentFile <= (totalEntries / entriesPerFile)) {
+      console.log(`File ${currentFile} generating. Overall progress: ${Math.round((currentId / (totalEntries + 100)) * 100)}%`);
+
+      fileWriteStream = fs.createWriteStream(path.join(__dirname, `/data/gallerydata/gallerydata${currentFile}.csv`));
+      currentFile += 1;
+      firstRow = true;
+    }
+
+    if (currentId > totalEntries) {
+      console.log(`All files successfully created! Overall progress: 100%`);
       const end = new Date().getTime();
-      console.log('Duration: ', (end - start) / 1000, ' seconds');
+      console.log(`Time elapsed: ${(end - start) / 1000} seconds.`);
       return;
     }
-    // const proceed = fileWriteStream.write(JSON.stringify(jsonGen(currentId)) + '\n');
-    const proceed = fileWriteStream.write(csvGen(currentId));
+
+    const proceed = fileWriteStream.write(csvGen2(currentId));
+    firstRow = false;
     if (proceed) {
-      writeChunksToFile(timesRun + 1);
+      currentId += 1;
+      writeToFile();
     } else {
       fileWriteStream.once('drain', () => {
-        writeChunksToFile(timesRun + 1);
+        currentId += 1;
+        writeToFile();
       });
     }
   };
 
-  writeChunksToFile(0);
+  writeToFile();
 };
 
-writeDataToFile(10000000);
+createFiles(10000000, 200000);
--- db/scripts/seedDB.js
+++ db/scripts/seedDB.js
@@ -1,193 +1,58 @@
-const mongoose = require('mongoose');
-const faker = require('faker');
-const db = require('../index.js');
-let count = 0;
+const { Client } = require('pg');
+const fs = require('fs');
+const path = require('path');
 
-const listingGenerator = function() {
-  // bali
-  var photoArr = [];
-  for (var i = 1; i < 28; i++) {
-    photoArr.push({url: `http://d13grdp3a2v9nw.cloudfront.net/bali/bali${i}.jpg`, caption:faker.lorem.sentence()});
-    var arr = photoArr.map(el => el);
+const createTableString = 'CREATE TABLE gallery( roomid INT NOT NULL, roomname TEXT NOT NULL, uniqId TEXT NOT NULL, photoURL TEXT NOT NULL, photoCaption TEXT NOT NULL)';
+const currDir = 'gallerydata';
+const filePath = path.join(__dirname, '/data/gallerydata');
+const start = new Date().getTime();
+let lengthOfDir = 0;
+
+
+fs.readdir(`./data/${currDir}`, (err, files) => {
+  if (err) {
+    throw err;
   }
-  var listing = new db.Listing ({_id: count, photo: arr});
-  listing.save(response => {
-    db.connect.close();
+  lengthOfDir = files.length - 1;
+  console.log(`There are ${lengthOfDir} files in the current directory`);
+});
+
+const client = new Client({
+  host: 'localhost',
+  database: 'wbnb',
+});
+
+const populateDb = () => {
+  return new Promise((resolve, reject) => {
+    const populateNextTable = (number) => {
+      if (number > lengthOfDir) {
+        resolve();
+      }
+      const csvToLoad = `COPY gallery FROM '${path.join(__dirname, `/data/gallerydata/gallerydata${number}.csv`)}' DELIMITER ',' CSV HEADER`;
+
+      client.query(csvToLoad)
+        .then(() => {
+          console.log(`${number}.csv successfully loaded!`);
+          console.log(`${(new Date().getTime() - start) / 1000} seconds have elapsed!\n\n`);
+        })
+        .then(() => populateNextTable(number + 1))
+        .catch(err => reject(err));
+    };
+    populateNextTable(1);
   });
-  count++;
-  //artisan
-  photoArr = [];
-  for (var i = 0; i < 16; i++) {
-    photoArr.push({url: `http://d13grdp3a2v9nw.cloudfront.net/artisan/art${i}.jpg`, caption:faker.lorem.sentence()});
-    var arr = photoArr.map(el => el);
-  }
-  var listing = new db.Listing ({_id: count, photo: arr});
-  listing.save(response => {
-    db.connect.close();
-  });
-  count++;
-  //cabana
-  photoArr = [];
-  for (var i = 0; i < 11; i++) {
-    photoArr.push({url: `http://d13grdp3a2v9nw.cloudfront.net/cabana/cabana${i}.jpg`, caption:faker.lorem.sentence()});
-    var arr = photoArr.map(el => el);
-  }
-  var listing = new db.Listing ({_id: count, photo: arr});
-  listing.save(response => {
-    db.connect.close();
-  });
-  count++;
-  //corner
-  photoArr = [];
-  for (var i = 0; i < 13; i++) {
-    photoArr.push({url: `http://d13grdp3a2v9nw.cloudfront.net/corner/corner${i}.jpg`, caption:faker.lorem.sentence()});
-    var arr = photoArr.map(el => el);
-  }
-  var listing = new db.Listing ({_id: count, photo: arr});
-  listing.save(response => {
-    db.connect.close();
-  });
-  count++;
-  //grey
-  photoArr = [];
-  for (var i = 0; i < 13; i++) {
-    photoArr.push({url: `http://d13grdp3a2v9nw.cloudfront.net/grey/grey${i}.jpg`, caption:faker.lorem.sentence()});
-    var arr = photoArr.map(el => el);
-  }
-  var listing = new db.Listing ({_id: count, photo: arr});
-  listing.save(response => {
-    db.connect.close();
-  });
-  count++;
-  //iceland
-  photoArr = [];
-  for (var i = 0; i < 15; i++) {
-    photoArr.push({url: `http://d13grdp3a2v9nw.cloudfront.net/iceland/iceland${i}.jpg`, caption:faker.lorem.sentence()});
-    var arr = photoArr.map(el => el);
-  }
-  var listing = new db.Listing ({_id: count, photo: arr});
-  listing.save(response => {
-    db.connect.close();
-  });
-  count++;
-  //luxury
-  photoArr = [];
-  for (var i = 0; i < 17; i++) {
-    photoArr.push({url: `http://d13grdp3a2v9nw.cloudfront.net/luxury/luxury${i}.jpg`, caption:faker.lorem.sentence()});
-    var arr = photoArr.map(el => el);
-  }
-  var listing = new db.Listing ({_id: count, photo: arr});
-  listing.save(response => {
-    db.connect.close();
-  });
-  count++;
-  //nautica
-  photoArr = [];
-  for (var i = 0; i < 16; i++) {
-    photoArr.push({url: `http://d13grdp3a2v9nw.cloudfront.net/nautica/nautica${i}.jpg`, caption:faker.lorem.sentence()});
-    var arr = photoArr.map(el => el);
-  }
-  var listing = new db.Listing ({_id: count, photo: arr});
-  listing.save(response => {
-    db.connect.close();
-  });
-  count++;
-  //outdoor
-  photoArr = [];
-  for (var i = 0; i < 19; i++) {
-    photoArr.push({url: `http://d13grdp3a2v9nw.cloudfront.net/outdoor/outdoor${i}.jpg`, caption:faker.lorem.sentence()});
-    var arr = photoArr.map(el => el);
-  }
-  var listing = new db.Listing ({_id: count, photo: arr});
-  listing.save(response => {
-    db.connect.close();
-  });
-  count++;
-  //relax
-  photoArr = [];
-  for (var i = 0; i < 13; i++) {
-    photoArr.push({url: `http://d13grdp3a2v9nw.cloudfront.net/relax/relax${i}.jpg`, caption:faker.lorem.sentence()});
-    var arr = photoArr.map(el => el);
-  }
-  var listing = new db.Listing ({_id: count, photo: arr});
-  listing.save(response => {
-    db.connect.close();
-  });
-  count++;
-  //sunny
-  photoArr = [];
-  for (var i = 0; i < 24; i++) {
-    photoArr.push({url: `http://d13grdp3a2v9nw.cloudfront.net/sunny/sunny${i}.jpg`, caption:faker.lorem.sentence()});
-    var arr = photoArr.map(el => el);
-  }
-  var listing = new db.Listing ({_id: count, photo: arr});
-  listing.save(response => {
-    db.connect.close();
-  });
-  count++;
-  //terrace
-  photoArr = [];
-  for (var i = 0; i < 18; i++) {
-    photoArr.push({url: `http://d13grdp3a2v9nw.cloudfront.net/terrace/terrace${i}.jpg`, caption:faker.lorem.sentence()});
-    var arr = photoArr.map(el => el);
-  }
-  var listing = new db.Listing ({_id: count, photo: arr});
-  listing.save(response => {
-    db.connect.close();
-  });
-  count++;
-  //theatre
-  photoArr = [];
-  for (var i = 0; i < 14; i++) {
-    photoArr.push({url:`http://d13grdp3a2v9nw.cloudfront.net/theatre/theatre${i}.jpg`,caption: faker.lorem.sentence()});
-    var arr = photoArr.map(el => el);
-  }
-  var listing = new db.Listing ({_id: count, photo: arr});
-  listing.save(response => {
-    db.connect.close();
-  });
-  count++;
-  //treehouse
-  photoArr = [];
-  for (var i = 0; i < 34; i++) {
-    photoArr.push({url:`http://d13grdp3a2v9nw.cloudfront.net/treehouse/treehouse${i}.jpg`,caption: faker.lorem.sentence()});
-    var arr = photoArr.map(el => el);
-  }
-  var listing = new db.Listing ({_id: count, photo: arr});
-  listing.save(response => {
-    db.connect.close();
-  });
-  count++;
-  //villa
-  photoArr = [];
-  for (var i = 0; i < 14; i++) {
-    photoArr.push({url:`http://d13grdp3a2v9nw.cloudfront.net/villa/villa${i}.jpg`, caption:faker.lorem.sentence()});
-    var arr = photoArr.map(el => el);
-  }
-  var listing = new db.Listing ({_id: count, photo: arr});
-  listing.save(response => {
-    db.connect.close();
-  });
-  count++;
-  //winter
-  photoArr = [];
-  for (var i = 0; i < 25; i++) {
-    photoArr.push({url:`http://d13grdp3a2v9nw.cloudfront.net/winter/winter${i}.jpg`, caption:faker.lorem.sentence()});
-    var arr = photoArr.map(el => el);
-  }
-  var listing = new db.Listing ({_id: count, photo: arr});
-  listing.save(response => {
-    db.connect.close();
-  });
-  count++;
 };
 
-const seedDB = () => {
-  console.log('seeding started');
-  for (let i = 0; i < 8; i++) {
-    listingGenerator();
-  }
-  console.log('seeding finished');
-};
 
-seedDB();
+client.connect()
+  .then(() => console.log('Connected to postgreSQL!'))
+  .then(() => client.query('DROP TABLE IF EXISTS gallery'))
+  .then(() => client.query(createTableString))
+  .then(result => console.log('Table successfully created!', result))
+  .then(() => populateDb())
+  .then(() => console.log('Database successfully populated! Creating Index on "roomid"...'))
+  .then(() => client.query('CREATE INDEX roomid_index ON gallery (roomid)'))
+  .then(() => {
+    console.log(`\n\n\n\n————— Seeding completed in ${(new Date().getTime() - start) / 1000} seconds —————\n\n\n\n`);
+    process.exit();
+  })
+  .catch(err => console.log('There was an unknown error.', err));
